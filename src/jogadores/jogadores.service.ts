09c51198f6e0b3e87e31dc5986bb7241929af37d
Alintana <alintana18@gmail.com>
2022-08-11 23:44:39 +0100

    Commit de jogadores

--- src/jogadores/jogadores.service.ts
+++ src/jogadores/jogadores.service.ts
@@ -1,4 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { v4 as uuid } from 'uuid';
+
+import { CriarJogadorDto } from './dtos/criar-jogador.dto';
+import { Jogador } from './interfaces/jogador.interface';
 
 @Injectable()
-export class JogadoresService {}
+export class JogadoresService {
+  private jogadores: Jogador[] = [];
+
+  private readonly logger = new Logger(JogadoresService.name);
+
+  async criarAtualizarJogador(criarJogadorDto: CriarJogadorDto): Promise<void> {
+    this.logger.log(`Cria jogador Dto: ${criarJogadorDto}`);
+    await this.criar(criarJogadorDto);
+  }
+
+  private async criar(criarJogadorDto: CriarJogadorDto): Promise<void> {
+    const { nome, email, telefoneCelular } = criarJogadorDto;
+
+    const jogador: Jogador = {
+      _id: uuid(),
+      email,
+      nome,
+      telefoneCelular,
+      ranking: `A`,
+      posicaoRanking: 1,
+      urlFotoJogador: `www.foto.jogador`,
+    };
+
+    this.jogadores.push(jogador);
+  }
+}
