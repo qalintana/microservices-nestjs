30788d095d9a3da5a9db7d42f9293578ee871458
Alintana <alintana18@gmail.com>
2022-08-14 21:23:26 +0100

    schema de categorias

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { JogadoresModule } from './jogadores/jogadores.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { CategoriasModule } from './categorias/categorias.module';
 
 @Module({
   imports: [
     JogadoresModule,
     MongooseModule.forRoot('mongodb://localhost:27017/apijogadores'),
+    CategoriasModule,
   ],
 })
 export class AppModule {}
